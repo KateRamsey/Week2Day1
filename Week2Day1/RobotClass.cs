479644c16a22ee73212203b2c899e978ef522b52
Kate Ramsey <kateramsey@live.com>
2016-03-07 13:35:22 -0600

    Built Robot class

--- Week2Day1/RobotClass.cs
+++ Week2Day1/RobotClass.cs
@@ -2,7 +2,24 @@
 
 public class Robot
 {
-	public Robot()
-	{  
-	}
+    public Robot()
+    {
+        this.Running = false;
+        this.IsTerminator = true;
+    }
+    public bool Running;
+    public bool IsTerminator;
+
+    public void StartUp()
+    {
+        Console.WriteLine("Started...");
+        this.Running = true;
+    }
+
+    public void ShutDown()
+    {
+        Console.WriteLine("Shutdown...");
+        this.Running = false;
+    }
+
 }
