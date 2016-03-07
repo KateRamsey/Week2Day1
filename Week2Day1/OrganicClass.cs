349d211cb6c114d8406d673d2dc0d99b5bd68dbd
Kate Ramsey <kateramsey@live.com>
2016-03-07 13:42:01 -0600

    create IsOrganic abstract class

---
+++ Week2Day1/OrganicClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public abstract class IsOrganic
+{
+	public bool ASleep { get; set;}
+    public void GoToSleep()
+    {
+        this.ASleep = true;
+    }
+
+    public void WakeUp()
+    {
+        this.ASleep = false;
+    }
+
+    public void Eat(string food)
+    {
+        Console.WriteLine($"Yum, I ate {food}");
+    }
+
+
+}
